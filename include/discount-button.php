4b432508dd0369e293b55b63d500296c9c7f7799
Zainul khan <zainulkhan032@gmail.com>
2025-06-11 10:32:01 +0530

    export old site

---
+++ include/discount-button.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="assets/css/module-css/discount-button.css">
+
+<div class="discount-btn">
+    <a href="#">
+        <img src="assets/images/discount-offer-3.png" alt="Special Discount" class="discount-img">
+    </a>
+</div>
