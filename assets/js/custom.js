13670237d79c744195e05b9a1a082065c7f05b0c
Zainul khan <zainulkhan032@gmail.com>
2025-06-19 16:10:27 +0530

    service section builded

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -23,22 +23,7 @@
 
 
 
-// Header fixed JS and Scroll to Top
-function headerStyle() {
-  if ($(".main-header").length) {
-    var windowpos = $(window).scrollTop();
-    var siteHeader = $(".main-header");
-    var scrollLink = $(".scroll-top");
-    if (windowpos >= 200) {
-      siteHeader.addClass("fixed-header");
-      scrollLink.fadeIn(300);
-    } else {
-      siteHeader.removeClass("fixed-header");
-      scrollLink.fadeOut(300);
-    }
-  }
-}
-headerStyle();
+
 
 //Mobile Nav Hide Show
 if ($(".mobile-menu").length) {
--- header.js
+++ header.js
@@ -1,15 +1,9 @@
 //Hide Loading Box (Preloader)
-// function handlePreloader() {
-//   if ($(".loader-wrap").length) {
-//     $(".loader-wrap").delay(1000).fadeOut(500);
-//   }
-// }
-
-// if ($(".preloader-close").length) {
-//   $(".preloader-close").on("click", function () {
-//     $(".loader-wrap").delay(200).fadeOut(500);
-//   });
-// }
+function handlePreloader() {
+  if ($(".loader-wrap").length) {
+    $(".loader-wrap").delay(500).fadeOut(500);
+  }
+}
 const letters = document.querySelectorAll(
   ".handle-preloader .animation-preloader .txt-loading .letters-loading"
 );
@@ -19,3 +13,20 @@ letters.forEach((el, index) => {
   el.style.setProperty("--delay", `${delay}s`);
   el.style.setProperty("animation-delay", `${delay}s`);
 });
+
+// Header fixed JS and Scroll to Top
+function headerStyle() {
+  if ($(".main-header").length) {
+    var windowpos = $(window).scrollTop();
+    var siteHeader = $(".main-header");
+    var scrollLink = $(".scroll-top");
+    if (windowpos >= 200) {
+      siteHeader.addClass("fixed-header");
+      scrollLink.fadeIn(300);
+    } else {
+      siteHeader.removeClass("fixed-header");
+      scrollLink.fadeOut(300);
+    }
+  }
+}
+headerStyle();
