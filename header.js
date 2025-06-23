8f65d68f8f1f55159b9b6da7f6f31b08c813ec70
Zainul khan <zainulkhan032@gmail.com>
2025-06-23 17:16:23 +0530

    home page desktop complete

--- header.js
+++ header.js
@@ -1,21 +1,3 @@
-//Hide Loading Box (Preloader)
-function handlePreloader() {
-  if ($(".loader-wrap").length) {
-    $(".loader-wrap").delay(500).fadeOut(500);
-  }
-}
-window.onload = function () {
-  handlePreloader();
-};
-const letters = document.querySelectorAll(
-  ".handle-preloader .animation-preloader .txt-loading .letters-loading"
-);
-letters.forEach((el, index) => {
-  const delay = 0.2 * (index + 1); // 0.2s, 0.4s, ...
-  el.style.setProperty("--delay", `${delay}s`);
-  el.style.setProperty("animation-delay", `${delay}s`);
-});
-
 // Header fixed JS and Scroll to Top
 function headerStyle() {
   if ($(".main-header").length) {
@@ -31,4 +13,8 @@ function headerStyle() {
     }
   }
 }
-headerStyle();
+jQuery(window).on("scroll", function () {
+  (function ($) {
+    headerStyle();
+  })(jQuery);
+});
