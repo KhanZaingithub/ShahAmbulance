93d6d3cec3c88ba568e0c2e430e0fd2020bdd6de
Zainul khan <zainulkhan032@gmail.com>
2025-06-30 18:27:58 +0530

    service page added

--- header.js
+++ header.js
@@ -4,6 +4,24 @@
     =============================================*/
   var $body = $("body");
 
+  //Hide Loading Box (Preloader)
+  function handlePreloader() {
+    if ($(".loader-wrap").length) {
+      $(".loader-wrap").delay(500).fadeOut(500);
+    }
+  }
+  window.onload = function () {
+    handlePreloader();
+  };
+  const letters = document.querySelectorAll(
+    ".handle-preloader .animation-preloader .txt-loading .letters-loading"
+  );
+  letters.forEach((el, index) => {
+    const delay = 0.2 * (index + 1); // 0.2s, 0.4s, ...
+    el.style.setProperty("--delay", `${delay}s`);
+    el.style.setProperty("animation-delay", `${delay}s`);
+  });
+
   $(window).on("scroll", function () {
     if ($(".header-area").length) {
       var windowpos = $(window).scrollTop();
